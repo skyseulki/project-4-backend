abc964fdbb2796579dc3ce97c192277018d8c302
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-05-28 11:15:30 -0700

    posts model in progress

---
+++ resources/posts.py
@@ -0,0 +1,93 @@
+from flask import jsonify, Blueprint, abort
+from flask_restful import (Resource, Api, reqparse, fields, marshal, marshal_with, url_for)
+
+import models
+
+post_fields = {
+    'id': fields.Integer,
+    'title': fields.String,
+    'review': fields.String
+}
+
+class PostList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        self.reqparse.add_argument(
+            'title',
+            required=False,
+            help='No title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'review',
+            required=False,
+            help='No review provided',
+            location=['form', 'json']
+        )
+        super().__init__()
+    
+    def get(self):
+        posts = [marshal(post, post_fields)
+                    for post in models.Post.select()]
+        return {'posts': posts}
+    
+    @marshal_with(post_fields)
+    def post(self):
+        args = self.reqparse.parse_args()
+        post = models.Post.create(**args)
+        return post
+
+class Post(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=False,
+            help='No title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'review',
+            required=False,
+            help='No review provided',
+            location=['form', 'json']
+        )
+        super().__init__()
+    
+    @marshal_with(post_fields)
+    def get(self, id):
+        try:
+            post = models.Post.get(models.Post.id==id)
+        except models.Post.DoesNotExist:
+            abort (404)
+        else:
+            return (post, 200)
+    
+    @marshal_with(post_fields)
+    def put(self, id):
+        args = self.reqparse.parse_args()
+        query = models.Post.update(**args).where
+        (models.Post.id==id)
+        query.execute()
+        return (models.Post.get(models.Post.id==id),
+        200)
+    
+    def delete(self, id):
+        query = models.Post.delete().where
+        (models.Post.id==id)
+        query.execute()
+        return 'post deleted'
+
+posts_api = Blueprint('resources.posts', __name__)
+api = Api(posts_api)
+
+api.add_resource(
+    PostList,
+    '/posts'
+)
+
+app.add_resource(
+    Post,
+    '/post/<int:id>'
+)
