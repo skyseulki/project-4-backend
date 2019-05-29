4094cd7e04d095ad63dc9497486164b0e50a9080
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-05-29 11:01:33 -0700

    added the login backend

--- resources/users.py
+++ resources/users.py
@@ -3,7 +3,7 @@ import json
 from flask import jsonify, Blueprint, abort, make_response
 from flask_restful import (Resource, Api, reqparse, inputs, fields, marshal, marshal_with, url_for)
 from flask_login import login_user, logout_user, login_required, current_user
-
+from flask_bcrypt import check_password_hash
 import models
 
 user_fields = {
@@ -75,6 +75,30 @@ class User(Resource):
             location=['form', 'json']
         )
         super().__init__()
+
+    def post(self):
+        try:
+            args = self.reqparse.parse_args()
+            user = models.User.get(models.User.username==args['username'])
+            if(user):
+                if(check_password_hash(user.password, args['password'])):
+                    return make_response(
+                        json.dumps({
+                            'user': marshal(user, user_fields),
+                            'success': True
+                        }), 200)
+
+                else:
+                    return make_response(
+                        json.dumps({
+                            'message': 'incorrect password'
+                        }), 200)
+        except models.User.DoesNotExist:
+            return make_response(
+                json.dumps({
+                    'message': 'Username does not exist'
+                }), 200)
+
 
     def put(self, id):
         return jsonify({'username': 'Franklin'})
